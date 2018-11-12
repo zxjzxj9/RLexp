f3b7497f980224379061c0538513b50353a51529
Victor Zhang <zxjzxj9@gmail.com>
2018-11-12 23:01:32 +0800

    Add Policy base class, add some statis to the Bandit class

--- ex01_n_armed_bandit/bandit.h
+++ ex01_n_armed_bandit/bandit.h
@@ -19,13 +19,24 @@ public:
         for(int i=0; i<n_arms; i++) {
             bandit.push_back(dist(rg));
         }
+        reward = 0;
+        count = 0;
     }
 
-    // Sample from the bandits
+    // Sample from the bandits, get a reward
     float sample(size_t bandit_idx, float sigma = 1.0) {
         float mu = bandit[bandit_idx];
         std::normal_distribution<float> dist(mu, sigma);
-        return dist(rg);
+        float ret = dist(rg);
+        reward += ret;
+        count += 1;
+        return ret;
+    }
+
+    // Get average reward
+    float avg_reward() {
+        if (count == 0) return 0.0;
+        return reward/ static_cast<float>(count);
     }
 
     // Get the argmax of bandits
@@ -45,6 +56,8 @@ private:
     int n_arms;
     float mu;
     float sigma;
+    float reward;
+    int count;
     // std::random_device rd;
     //std::mt19937 rg; //{std::random_device{}()};
     std::mt19937 rg{std::random_device{}()};
@@ -52,7 +65,23 @@ private:
     std::vector<float> bandit;
 };
 
+// Define a basic policy class to simulate the bandit
 class Policy {
 
+public:
+    //Policy(const Bandit& bandit): bandit(bandit) {}
+    virtual void simulate(int n) = 0;
+
+private:
+    // Perform a single run
+    virtual void run(int n) = 0;
+
 };
+
+
+// Define greedy RL policy
+class GreedyPolicy: Policy {
+
+};
+
 #endif //EX01_N_ARMED_BANDIT_BANDIT_H
