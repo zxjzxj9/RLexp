1150f21c9159ddd16bb52db6df98b05b9ca4ba2b
Victor Zhang <zxjzxj9@gmail.com>
2018-11-17 23:33:27 +0800

    Add some figure plots

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ The is the code for the **Experiments of Reinforcement Learning**, which will be
 	- This problem contains a bandit with n arms, each generate a normal distribution reward.
 	- Three policies will be implemented: i.e. *Greedy Policy*, *ε-Greedy Policy*, *Gradient Policy*.
 	- Simulation results will be displayed by Python.
-
+	- Some basic results can be viewed as follows
+	![Fig 1.1 Simulate Result](./ex01_n_armed_bandit/imgs/result.png)
+	- It can be seen that *ε-Greedy Policy* is apprently better than *Greedy Policy*
---
+++ ex01_n_armed_bandit/imgs/data_plot.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import numpy as np
+sns.set(style="darkgrid")
+
+
+greedy = np.genfromtxt("./greed.dat", dtype=(np.int32, np.float, np.float))
+#greedy = np.genfromtxt("./greed.dat", dtype=None)
+#greedy = np.genfromtxt("./greed.dat", dtype=[('f0', '<i4'), ('f1', '<f8'), ('f2', '<f8')])
+idx = [t[0] for t in greedy]
+reward = [t[1] for t in greedy]
+prob = [t[2] for t in greedy]
+#print(idx, reward, prob)
+plt.plot(idx, reward, color='r', ls="--", label="greedy_reward")
+plt.plot(idx, prob, color='r', ls="-", label="greedy_prob")
+
+greedy = np.genfromtxt("./egreed.dat", dtype=(np.int32, np.float, np.float))
+idx = [t[0] for t in greedy]
+reward = [t[1] for t in greedy]
+prob = [t[2] for t in greedy]
+
+plt.plot(idx, reward, color='b', ls="--", label="$\epsilon$-greedy_reward")
+plt.plot(idx, prob, color='b', ls="-", label="$\epsilon$-greedy_prob")
+plt.legend()
+plt.xlabel("step")
+plt.savefig("result.png")
+plt.show()
