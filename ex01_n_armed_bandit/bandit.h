2643107aca532fd487814559c6827293f596716c
Victor Zhang <zxjzxj9@gmail.com>
2018-11-18 23:04:30 +0800

    Add gradient based bandit

--- ex01_n_armed_bandit/bandit.h
+++ ex01_n_armed_bandit/bandit.h
@@ -10,6 +10,7 @@
 #include <random>
 #include <algorithm>
 #include <iomanip>
+#include <cmath>
 
 class Bandit {
 
@@ -26,7 +27,7 @@ public:
     }
 
     // Sample from the bandits, get a reward
-    void sample(size_t bandit_idx, float sigma = 1.0) {
+    virtual void sample(size_t bandit_idx, float sigma = 1.0) {
         cnt_tot += 1;
         float mu = bandit[bandit_idx];
         std::normal_distribution<float> dist(mu, sigma);
@@ -70,7 +71,7 @@ public:
         std::cout<<std::endl;
     }
 
-private:
+protected:
     int n_arms;
     int cnt_tot;
     float mu;
@@ -81,6 +82,72 @@ private:
     std::vector<float> reward;
     std::vector<int> cnt;
     //std::vector<int> cnt;
+};
+
+
+// Gradient based Bandit
+class GBandit: public Bandit {
+public:
+    GBandit(int n_arms, float mu = 0.0, float sigma = 1.0, float lr=0.1):
+            Bandit(n_arms, mu, sigma), lr(lr) {
+        for(int i=0; i<n_arms; i++) {
+            H.push_back(0);
+        }
+    }
+
+    // return sampled index according to saved H
+    int sample_idx() {
+        //std::unique_ptr<float[]> prob{new float[Bandit::n_arms]};
+        auto prob = std::unique_ptr<float[]>{new float[Bandit::n_arms]};
+        std::copy(std::begin(H), std::end(H), prob.get());
+        std::transform(prob.get(), prob.get()+Bandit::n_arms, prob.get(), [=](float x)->float{return exp(x);});
+        float sum = std::accumulate(prob.get(), prob.get()+Bandit::n_arms, 0);
+        // Normalize
+        std::transform(prob.get(), prob.get()+Bandit::n_arms, prob.get(), [=](float x)->float{return x/sum;});
+        // https://stackoverflow.com/questions/4437250/choose-list-variable-given-probability-of-each-variable
+        // Sample from distribution
+        std::uniform_real_distribution<float> dist(0.0, 1.0);
+        float p = dist(Bandit::rg);
+        for(int i=0; i<Bandit::n_arms; i++) {
+            p -= prob[i];
+            if(p<=0) {
+                probt = prob[i];
+                return i;
+            }
+        }
+        probt = prob[0];
+        return 0;
+    }
+
+    void sample(float sigma=1.0) {
+        cnt_tot += 1;
+        size_t bandit_idx = sample_idx();
+        float mu = bandit[bandit_idx];
+        std::normal_distribution<float> dist(mu, sigma);
+        float ret = dist(rg);
+        update(ret, bandit_idx);
+        reward_tot += ret;
+    }
+
+    virtual void update(float val, int bandit_idx) {
+        float avg_reward = get_avg_reward();
+        cnt[bandit_idx] += 1;
+        reward[bandit_idx] = reward[bandit_idx] +
+                             (val - reward[bandit_idx])/static_cast<float>(cnt[bandit_idx]);
+        H[bandit_idx] = H[bandit_idx] + lr*(val - avg_reward)*(1-probt);
+        for(int i=0; i<Bandit::n_arms; i++) {
+            if(i == bandit_idx) continue;
+            H[i] = H[i] - lr*(val - avg_reward)*probt;
+        }
+    }
+
+
+
+private:
+    // define learning rate
+    float lr;
+    float probt;
+    std::vector<float> H;
 };
 
 // Define a basic policy class to simulate the bandit
@@ -179,5 +246,6 @@ private:
     std::mt19937 rg{std::random_device{}()};
     std::uniform_real_distribution<> dist{0.0, 1.0};
 };
+
 
 #endif //EX01_N_ARMED_BANDIT_BANDIT_H
