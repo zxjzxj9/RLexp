2173f709515ee46d87de9ba76c7d0b437eb3e30d
Victor Zhang <zxjzxj9@gmail.com>
2018-11-25 23:34:11 +0800

    Add some code to gridworld environment

--- ex02_gridworld/gridworld.h
+++ ex02_gridworld/gridworld.h
@@ -28,6 +28,9 @@ public:
     GridWorld(int n, int m, std::map<std::pair<int, int>, float> reward,
               std::map<std::pair<int, int>, std::pair<int, int> > teleport
         ): n(n), m(m) {
+        // initialize all the reward to 0, teleport to -1 (means no teleportation)
+        std::fill(this->reward.get(), this->reward.get() + n*m - 1, 0);
+        std::fill(this->teleport.get(), this->teleport.get() + n*m - 1, -1);
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 this->reward[i*m + j] = reward[std::pair<int, int>(i, j)];
@@ -38,15 +41,22 @@ public:
     }
 
     // step a move, return the reward, and teleport
-    float move(Direction d) {
+    // always set out-of-boundary reward -1
+    float move(Direction d, std::pair<int, int>& coord) {
         switch(d) {
             case LEFT:
+                if(coord.first == 0) return -1;
+                coord.first -= 1;
+                //int t = coord.first*m + coord.second;
                 break;
             case RIGHT:
+                if(coord.first == m - 1) return -1;
                 break;
             case UP:
+                if(coord.second == 0) return -1;
                 break;
             case DOWN:
+                if(coord.second == n - 1) return -1;
                 break;
             default:
                 throw std::runtime_error("No such dircetion!");
