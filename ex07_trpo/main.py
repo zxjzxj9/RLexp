b4d6126b8df52ff236bfbb1401b4956e2c5849be
Victor Zhang <zxjzxj9@gmail.com>
2020-11-04 19:35:53 +0800

    Change the model to make it work

--- ex07_trpo/main.py
+++ ex07_trpo/main.py
@@ -9,7 +9,7 @@ import gym
 import numpy as np
 from torch import optim, distributions
 
-from model import PolicyNet, ValueNet, list_to_tensor
+from model import PolicyNet, EnvSampler
 
 from torch.utils.tensorboard import SummaryWriter
 
@@ -27,116 +27,54 @@ env = gym.make("Pong-v0")
 # plt.imshow(np.array(obs), cmap='gray')
 # plt.savefig("test.png")
 
-MAXSTEP = 128
-BATCHSIZE = 16
-EPOCH = 1000000
+MAXSTEP = 32
+BATCHSIZE = 32
+EPOCH = 4000*100 # around ~4000 1 EPOCH in A3C paper
 GAMMA = 0.99
 
 NFRAMES = 4
 
-policy_net = PolicyNet()
-value_net = ValueNet()
+policy_net = PolicyNet(NFRAMES)
 
 policy_net.cuda()
-value_net.cuda()
-opt1 = optim.Adam(policy_net.parameters(), lr=1e-3)
-opt2 = optim.Adam(value_net.parameters(), lr=1e-3)
+opt = optim.RMSprop(policy_net.parameters(), lr=1e-3, alpha=0.99, eps=1e-5)
 
+sampler = EnvSampler(env, policy_net, NFRAMES, MAXSTEP, GAMMA)
 global_step = 0
+
 def train_step():
 
-    observ_batch = []
-    reward_batch = []
-    action_batch = []
-    discount_reward_batch = []
+    observ_batch, action_batch, discount_reward_batch, avg_reward = sampler.sample_n(BATCHSIZE)
 
-    # policy_net.cpu()
-    # value_net.cpu()
+    opt.zero_grad()
 
-    # mask =[]
-    for _ in range(BATCHSIZE):
-        observ = []
-        reward = []
-        action = []
-
-        obs = env.reset()
-        windows = [obs.copy()]*4
-        observ.append(list_to_tensor(windows))
-
-        for _ in range(MAXSTEP):
-            with torch.no_grad():
-                logits = policy_net(observ[-1].unsqueeze(0).cuda())
-                dist = distributions.Categorical(logits=logits)
-                act = dist.sample().cpu()
-                action.append(act.item())
-            obs, rwd, end, _ = env.step(act.item())
-            windows.pop(0)
-            windows.append(obs.copy())
-            observ.append(list_to_tensor(windows))
-            reward.append(rwd)
-            if end: break
-
-        # plt.imshow(np.array(obs), cmap='gray')
-        # plt.savefig("test.png")
-        # import sys; sys.exit()
-
-        with torch.no_grad():
-            observ = torch.stack(observ[:-1], dim=0).cuda()
-            reward = torch.tensor(reward, dtype=torch.float32).cuda()
-            action = torch.tensor(action).cuda()
-
-            discount_reward = reward.clone()
-            for idx in reversed(range(reward.size(0)-1)):
-                discount_reward[idx] += GAMMA*discount_reward[idx+1]
-
-        observ_batch.append(observ)
-        action_batch.append(action)
-        reward_batch.append(reward)
-        discount_reward_batch.append(discount_reward)
-
-    with torch.no_grad():
-        observ_batch = torch.cat(observ_batch, dim=0)
-        action_batch = torch.cat(action_batch, dim=0)
-        reward_batch = torch.cat(reward_batch, dim=0)
-        discount_reward_batch = torch.cat(discount_reward_batch, dim=0)
-    # print(observ_batch.shape, reward_batch.shape, action_batch.shape)
-    # import sys; sys.exit()
-
-    avg_reward = reward_batch.mean()
-    # print(avg_reward)
-
-    opt1.zero_grad()
-    opt2.zero_grad()
-
-    policy_pr = policy_net(observ_batch)
-    pr = policy_pr.softmax(dim=-1)
-    value_pr = value_net(observ_batch)
+    policy_pr, value_pr = policy_net(observ_batch)
+    pr = policy_pr.softmax(-1)
+    # print(policy_pr.shape, value_pr.shape, discount_reward_batch.shape)
 
     with torch.no_grad():
         advantage = (discount_reward_batch - value_pr).detach()
-    # print(policy_pr.shape, action_batch.shape)
-    # import sys; sys.exit()
     logprob = F.cross_entropy(policy_pr, action_batch, reduction='none')
-    loss1 = (logprob * advantage + 0.01*(pr*pr.log()).sum(-1)).mean()
-    # loss1 = (logprob * advantage).mean()
-    loss2 = (value_pr - discount_reward_batch).pow(2).mean()
-    loss1.backward()
-    loss2.backward()
+    # print(logprob.shape, advantage.shape)
+    # import sys; sys.exit()
+    loss = (logprob * advantage).mean() + 0.01*(pr*pr.log()).sum(-1).mean() + \
+           0.5*(value_pr - discount_reward_batch).pow(2).mean()
 
-    opt1.step()
-    opt2.step()
+    loss.backward()
+    torch.nn.utils.clip_grad_norm_(policy_net.parameters(), 0.5)
+
+    opt.step()
 
     global global_step
     global_step += 1
+    writer.add_scalar("Episode", sampler.episodes, global_step=global_step)
     writer.add_scalar("Reward", avg_reward.item(), global_step=global_step)
-    writer.add_scalar("Policy Loss", loss1.item(), global_step=global_step)
-    writer.add_scalar("Value Loss", loss2.item(), global_step=global_step)
+    writer.add_scalar("Total Loss", loss.item(), global_step=global_step)
+    writer.add_scalar("Average Value", value_pr.mean().item(), global_step=global_step)
 
-    return avg_reward.item(), loss1.item(), loss2.item()
+    return avg_reward.item(), loss.item()
 
 if __name__ == "__main__":
     for i in range(EPOCH):
-        print("Epoch: {:6d}, Avg_reward: {:12.6f}, PolicyNet Loss: {:12.6f}, ValueNet Loss: {:12.6f}".format(i+1, *train_step()), end="\r")
-
-    torch.save({"policy": policy_net.state_dict(), 
-                "value": value_net.state_dict()}, "model.pt")
+        print("Epoch: {:6d}, Avg_reward: {:12.6f}, PolicyNet Loss: {:12.6f}".format(i+1, *train_step()), end="\r")
+        if (i+1) % 4000 == 0:  torch.save({"policy": policy_net.state_dict()}, "model.pt")
--- ex07_trpo/model.py
+++ ex07_trpo/model.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from PIL import Image
 from torchvision import transforms
+from torch import optim, distributions
 
 trans = transforms.Compose([
     transforms.Resize((110, 84)),
@@ -24,7 +25,7 @@ def list_to_tensor(li):
     return li
 
 class PolicyNet(nn.Module):
-    def __init__(self, nframe = 4, nspace=6):
+    def __init__(self, nframe=4, nspace=6):
 
         super().__init__()
         self.net1 = nn.Sequential(
@@ -36,35 +37,116 @@ class PolicyNet(nn.Module):
         self.net2 = nn.Sequential(
              nn.Linear(32*9*9, 256),
              nn.ReLU(),
-             nn.Linear(256, nspace),
         )
+        self.pnet = nn.Linear(256, nspace)
+        self.vnet = nn.Linear(256, 1)
 
     def forward(self, x):
         x = self.net1(x)
         x = x.view(x.size(0), -1)
         x = self.net2(x)
-        return x
+        p = self.pnet(x)
+        v = self.vnet(x).squeeze(-1)
+        return p, v
 
-class ValueNet(nn.Module):
-    def __init__(self, nframe=4, nspace=6):
-        super().__init__()
-        self.net1 = nn.Sequential(
-            nn.Conv2d(nframe, 16, 8, stride=4),
-            nn.ReLU(),
-            nn.Conv2d(16, 32, 4, stride=2),
-            nn.ReLU(),
-        )
-        self.net2 = nn.Sequential(
-             nn.Linear(32*9*9, 256),
-             nn.ReLU(),
-             nn.Linear(256, 1),
-        )
+class EnvSampler(object):
 
-    def forward(self, x):
-        x = self.net1(x)
-        x = x.view(x.size(0), -1)
-        x = self.net2(x).squeeze(-1)
-        return x
+    def __init__(self, env, pnet, nframes=4, maxstep=128, gamma=0.99):
+
+        self.env = env
+        self.pnet = pnet
+        self.nframes = nframes
+        self.maxstep = maxstep
+        self.gamma = gamma
+        self.neps = 0 # number of episodes
+        self.reset()
+
+    @property
+    def episodes(self):
+        return self.neps
+
+    def reset(self):
+        obs = self.env.reset()
+        self.windows = [obs.copy()]*self.nframes
+
+    def sample(self):
+
+        observ_batch = []
+        reward_batch = []
+        action_batch = []
+        discount_reward_batch = []
+        avg_reward = 0.0
+        last_reward = 0.0
+        end = False
+
+        observ = [list_to_tensor(self.windows)]
+        action = []
+        reward = []
+
+        for _ in range(self.maxstep):
+            with torch.no_grad():
+                # print(observ[-1].shape)
+                logits, last_reward = self.pnet(observ[-1].unsqueeze(0).cuda())
+                dist = distributions.Categorical(logits=logits)
+                act = dist.sample().cpu()
+                action.append(act.item())
+
+            # obs = None
+            # rwd = 0.0
+            # for _ in range(self.nframes):
+            #     obs, r, end, _ = self.env.step(act.item())
+            #     rwd += r
+            obs, rwd, end, _ = self.env.step(act.item())
+
+            self.windows.pop(0)
+            self.windows.append(obs.copy())
+            observ.append(list_to_tensor(self.windows))
+            reward.append(rwd)
+            if end: break
+
+        observ = torch.stack(observ[:-1], dim=0).cuda()
+        reward = torch.tensor(reward, dtype=torch.float32).cuda()
+        action = torch.tensor(action).cuda()
+        avg_reward = reward.mean()
+
+        if not end:
+            with torch.no_grad():
+                # print(reward.shape)
+                # print(last_reward.shape)
+                # print(reward[-1], reward[-2])
+                # import sys; sys.exit()
+                reward[-1] = reward[-1] + self.gamma*last_reward.squeeze(0)
+
+        with torch.no_grad():
+            discount_reward = reward.clone()
+            for idx in reversed(range(reward.size(0)-1)):
+                discount_reward[idx] += self.gamma*discount_reward[idx+1]
+
+        if end:
+            self.neps += 1
+            self.reset()
+
+        return observ, action, discount_reward, avg_reward
+
+    def sample_n(self, n=1):
+        observ_batch = []
+        action_batch = []
+        discount_reward_batch = []
+        avg_reward_batch = []
+
+        with torch.no_grad():
+            for _ in range(n):
+                observ, action, discount_reward, avg_reward = self.sample()
+                observ_batch.append(observ)
+                action_batch.append(action)
+                discount_reward_batch.append(discount_reward)
+                avg_reward_batch.append(avg_reward)
+
+            observ_batch = torch.cat(observ_batch, 0)
+            action_batch = torch.cat(action_batch, 0)
+            discount_reward_batch = torch.cat(discount_reward_batch, 0)
+            avg_reward_batch = torch.stack(avg_reward_batch, 0).mean()
+        return observ_batch, action_batch, discount_reward_batch, avg_reward_batch
 
 
 if __name__ == "__main__":
