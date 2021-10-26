5684ca25086b7da37a1dbec9a1538a643373d6fd
Victor Zhang <zxjzxj9@gmail.com>
2021-10-26 23:38:57 +0800

    Add feature extraction net

--- ex13_mcts/model.py
+++ ex13_mcts/model.py
@@ -8,8 +8,13 @@ class PVNet(nn.Module):
     def __init__(self, in_chan, mid_chan, out_chan):
         super().__init__()
 
-        self.featnet = None
-
+        mods = [nn.Conv2d(in_chan, mid_chan[0], 3, 1), nn.ReLU(inplace=True)]
+        for cin, cout in zip(min_chan[:-1], [1:]):
+            mods.append(nn.Conv2d(cin, cout, 3, 1)
+            mods.append(nn.ReLU(inplace=True))
+        mods.append(nn.Conv2d(mid_chan[-1], out_chan, 3, 1))
+        mods.append(nn.ReLU(inplace=True))
+        self.featnet = nn.Sequential(*mods) 
         self.policy = None
         self.value = None
 
